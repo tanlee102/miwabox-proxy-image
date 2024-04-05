54b6d54288ac7ee46dc642cea1a9279aa8e2519c
LE QUOC TAN <letanprox@gmail.com>
2024-04-05 13:28:34 +0700

    B

--- app/[id]/route.js
+++ app/[id]/route.js
@@ -11,7 +11,7 @@ export async function GET(request, context) {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "GET, POST, OPTION",
-    }
+      }
       
       const store = getStore({
         name: 'image-store',
@@ -19,22 +19,21 @@ export async function GET(request, context) {
         token: 'nfp_nEoRVLqwwBCnV1aVTn9MCQV9juJKyiJD5f31',
       });
 
-
       var blob = null
       blob = await store.get(id, { type: 'blob' }); 
 
       if(blob){
-        return new NextResponse(blob, { status: 200, headers })
+        return new NextResponse(blob.stream(), { status: 200, headers })
       }else{
         const url = 'https://drive.google.com/uc?id=' + id + '&export=download';
         // const url = 'https://www.googleapis.com/drive/v3/files/'+req.nextUrl.searchParams.get("id")+'?key=AIzaSyCDEQ915m_RAEWxhOghge1sWUBO6cnROVI&alt=media'
   
         const response = await fetch(url, { cache: 'force-cache' });
         blob = await response.blob();
-
+        
         await store.set(id, blob, {type: 'blob'})
 
-        return new NextResponse(blob, { status: 200, headers })
+        return new NextResponse(blob.stream(), { status: 200, headers })
       }
 
     } catch (error) {
