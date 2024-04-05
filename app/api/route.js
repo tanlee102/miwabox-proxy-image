d88929f80b367c282bb33adbfb357f40017cc81c
LE QUOC TAN <letanprox@gmail.com>
2024-04-05 16:32:03 +0700

    U

--- app/api/route.js
+++ app/api/route.js
@@ -1,24 +1,24 @@
 import { google } from 'googleapis';
 import { NextResponse } from 'next/server'
-import { Readable } from 'stream';
+// import { Readable } from 'stream';
 
 
 export async function POST(request, context) {
 
 
     try {
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.CLIENT_ID, 
-            process.env.CLIENT_SECRET, 
-            'http://localhost:80'
-        );
-        oauth2Client.setCredentials({
-            refresh_token: process.env.REFRESH_TOKEN
-        });
-        const drive = google.drive({
-            version: 'v3',
-            auth: oauth2Client
-        });
+        // const oauth2Client = new google.auth.OAuth2(
+        //     process.env.CLIENT_ID, 
+        //     process.env.CLIENT_SECRET, 
+        //     'http://localhost:80'
+        // );
+        // oauth2Client.setCredentials({
+        //     refresh_token: process.env.REFRESH_TOKEN
+        // });
+        // const drive = google.drive({
+        //     version: 'v3',
+        //     auth: oauth2Client
+        // });
         
         // const formData = await request.formData();
         // const image = formData.get("image") instanceof File ? formData.get("image") : null;
@@ -39,9 +39,9 @@ export async function POST(request, context) {
         //     },
         // });
     
-        return NextResponse.json(image  , { status: 200 })   
+        return NextResponse.json("helo"  , { status: 200 })   
     } catch (error) {
-        return NextResponse.json({how:"shit"}  , { status: 400 })   
+        return NextResponse.json(error  , { status: 400 })   
     }
 
 }
