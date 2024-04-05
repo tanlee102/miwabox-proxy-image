ca82a00305fcadb2652aa5946bdc142de1d51226
LE QUOC TAN <letanprox@gmail.com>
2024-04-05 15:49:22 +0700

    U

--- app/api/route.js
+++ app/api/route.js
@@ -5,38 +5,43 @@ import { Readable } from 'stream';
 
 export async function POST(request, context) {
 
-    const oauth2Client = new google.auth.OAuth2(
-        process.env.CLIENT_ID, 
-        process.env.CLIENT_SECRET, 
-        'http://localhost:80'
-    );
-    oauth2Client.setCredentials({
-        refresh_token: process.env.REFRESH_TOKEN
-    });
-    const drive = google.drive({
-        version: 'v3',
-        auth: oauth2Client
-    });
+
+    try {
+        const oauth2Client = new google.auth.OAuth2(
+            process.env.CLIENT_ID, 
+            process.env.CLIENT_SECRET, 
+            'http://localhost:80'
+        );
+        oauth2Client.setCredentials({
+            refresh_token: process.env.REFRESH_TOKEN
+        });
+        const drive = google.drive({
+            version: 'v3',
+            auth: oauth2Client
+        });
+        
+        const formData = await request.formData();
+        const image = formData.get("image") instanceof File ? formData.get("image") : null;
     
-    const formData = await request.formData();
-    const image = formData.get("image") instanceof File ? formData.get("image") : null;
-
-    const imageBuffer = await new Response(image).arrayBuffer();
-
-    const imageStream = new Readable();
-    imageStream.push(Buffer.from(imageBuffer));
-    imageStream.push(null);
-
-    const response = await drive.files.create({
-        requestBody: {
-          name: 'mytan.jpeg', // This can be any name, and doesn't have to match the actual file
-        },
-        media: {
-          mimeType: 'image/jpeg',
-          body: imageStream, // Use the actual path to your file
-        },
-    });
-
-    return NextResponse.json(1  , { status: 200 })
+        const imageBuffer = await new Response(image).arrayBuffer();
+    
+        const imageStream = new Readable();
+        imageStream.push(Buffer.from(imageBuffer));
+        imageStream.push(null);
+    
+        const response = await drive.files.create({
+            requestBody: {
+              name: 'mytan.jpeg', // This can be any name, and doesn't have to match the actual file
+            },
+            media: {
+              mimeType: 'image/jpeg',
+              body: imageStream, // Use the actual path to your file
+            },
+        });
+    
+        return NextResponse.json(1  , { status: 200 })   
+    } catch (error) {
+        return NextResponse.json(error  , { status: 400 })   
+    }
 
 }
