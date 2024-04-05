0602c0ff262c02bdc09cf2f55eb13e4c9042e381
LE QUOC TAN <letanprox@gmail.com>
2024-04-05 14:36:31 +0700

    B

--- app/[id]/route.js
+++ app/[id]/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from 'next/server'
-import { getStore, getDeployStore } from "@netlify/blobs";
+import { getStore } from "@netlify/blobs";
 
 export async function GET(request, context) {
 
   try {
+
+      const password = String(request.nextUrl.searchParams.get("password"));
 
       const id = String(context.params.id).replace('.jpeg', '');
       const headers = {
@@ -13,31 +15,55 @@ export async function GET(request, context) {
         "Access-Control-Allow-Methods": "GET, POST, OPTION",
       }
       
+
       const store = getStore({
-        name: 'image-store',
+        name: 'images-store',
+        siteID: '30cc35f8-b6da-4d54-ade8-0d6e322e0b48',
+        token: 'nfp_nEoRVLqwwBCnV1aVTn9MCQV9juJKyiJD5f31',
+      });
+      const keys = getStore({
+        name: 'keys-store',
         siteID: '30cc35f8-b6da-4d54-ade8-0d6e322e0b48',
         token: 'nfp_nEoRVLqwwBCnV1aVTn9MCQV9juJKyiJD5f31',
       });
 
-      var blob = null
-      blob = await store.get(id, { type: 'blob' }); 
 
-      if(blob){
-        return new NextResponse(blob.stream(), { status: 200, headers })
+      var approve = false
+      if(password === String(process.env.PASSWORD)){
+        approve = true
+        await keys.set(id, 'true')
       }else{
-        const url = 'https://drive.google.com/uc?id=' + id + '&export=download';
-        // const url = 'https://www.googleapis.com/drive/v3/files/'+req.nextUrl.searchParams.get("id")+'?key=AIzaSyCDEQ915m_RAEWxhOghge1sWUBO6cnROVI&alt=media'
-  
-        const response = await fetch(url, { cache: 'force-cache' });
-        blob = await response.blob();
-        
-        await store.set(id, blob, {type: 'blob'})
+        let tmp = await keys.get(id)
+        if(tmp){
+          approve = true
+        }
+      }
 
-        return new NextResponse(blob.stream(), { status: 200, headers })
+
+      if(approve){
+        var blob = null
+        blob = await store.get(id, { type: 'blob' }); 
+  
+        if(blob){
+          return new NextResponse(blob.stream(), { status: 200, headers })
+        }else{
+          const url = 'https://drive.google.com/uc?id=' + id + '&export=download';
+          // const url = 'https://www.googleapis.com/drive/v3/files/'+id+'?key=AIzaSyCDEQ915m_RAEWxhOghge1sWUBO6cnROVI&alt=media'
+    
+          const response = await fetch(url, { cache: 'force-cache' });
+          if(response.status == 200){
+            blob = await response.blob();
+            await store.set(id, blob, {type: 'blob'})
+            return new NextResponse(blob.stream(), { status: 200, headers })
+          }else{
+            return NextResponse.json({ error: 'Not Found' }, { status: 404 });
+          }
+        }
+      }else{
+        return NextResponse.json({ error: 'Not Authencation' }, { status: 400 });
       }
 
     } catch (error) {
-      console.log(error)
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
 }
