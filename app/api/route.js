f28cbb5458a526302460b1aca2622a8d6fa74869
LE QUOC TAN <letanprox@gmail.com>
2024-04-19 17:33:18 +0700

    T

--- app/api/route.js
+++ app/api/route.js
@@ -25,7 +25,7 @@ export async function POST(request, context) {
 
 
         const headers = {
-            "Content-Type": "image/jpeg",
+            "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "GET, POST, OPTION",
