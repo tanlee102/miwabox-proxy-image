d4df319ad1f42b30fd71c3c66b89bf671b32818e
LE QUOC TAN <letanprox@gmail.com>
2024-05-03 18:18:50 +0700

    G

--- app/delete/blob/route.js
+++ app/delete/blob/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { getStore } from "@netlify/blobs";
 
-export async function DELETE(request, context) {
+export async function GET(request, context) {
 
     try {
         const headers = {
