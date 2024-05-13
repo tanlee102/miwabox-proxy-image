abae68962fd8963d5a9ef32878fd9f836ae92164
LE QUOC TAN <letanprox@gmail.com>
2024-05-13 23:29:09 +0700

    U

--- middleware.js
+++ middleware.js
@@ -38,7 +38,7 @@ export async function middleware(request) {
             "Content-Type": "application/json, text/plain, */*",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Methods": "GET, PUT, POST, OPTIONS",
         }
         return new NextResponse({ status: 200, headers });
     }
