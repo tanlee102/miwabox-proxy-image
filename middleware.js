9d3f88e8d751119f8b7660836f6ae85559deb18c
LE QUOC TAN <letanprox@gmail.com>
2024-05-13 23:11:16 +0700

    U

--- middleware.js
+++ middleware.js
@@ -15,23 +15,24 @@ export async function middleware(request) {
     console.log(headersList)
 
     if(myAuthorization){
-        const token = headersList.get('authorization').replace('Bearer ', '');
+        
+        const token = headersList.get('mytoken').replace('Bearer ', '');
         const secret = new TextEncoder().encode(process.env.MY_AUTH_KEY);
     
-        try {
-            const { payload: { email } } = await jose.jwtVerify(token, secret);
-            if (!listEmailApproved.includes(email)) {
-                throw new Error();
-            }
-        } catch(err) {
-            console.log(err);
-            return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
-                "Content-Type": "*",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-            }});
-        }
+        // try {
+        //     const { payload: { email } } = await jose.jwtVerify(token, secret);
+        //     if (!listEmailApproved.includes(email)) {
+        //         throw new Error();
+        //     }
+        // } catch(err) {
+        //     console.log(err);
+        //     return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
+        //         "Content-Type": "*",
+        //         "Access-Control-Allow-Origin": "*",
+        //         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        //         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        //     }});
+        // }
     }else if(request.method === 'OPTIONS'){
         const headers = {
             "Content-Type": "*",
