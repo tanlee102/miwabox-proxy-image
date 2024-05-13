8622d950b6cda10cb0c6a360a30120d6569fed3a
LE QUOC TAN <letanprox@gmail.com>
2024-05-13 22:19:34 +0700

    U

--- middleware.js
+++ middleware.js
@@ -10,24 +10,42 @@ const listEmailApproved = [
 export async function middleware(request) {
 
     const headersList = headers()
+    const myAuthorization = headersList.get('authorization');
+    
     console.log(headersList)
-    // const token = headersList.get('Authorization').replace('Bearer ', '');
-    // const secret = new TextEncoder().encode(process.env.MY_AUTH_KEY);
 
-    // try {
-    //     const { payload: { email } } = await jose.jwtVerify(token, secret);
-    //     if (!listEmailApproved.includes(email)) {
-    //         throw new Error();
-    //     }
-    // } catch(err) {
-    //     console.log(err);
-    //     return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
-    //         "Content-Type": "application/json",
-    //         "Access-Control-Allow-Origin": "*",
-    //         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    //         "Access-Control-Allow-Methods": "GET, POST", // If you're making POST requests
-    //     }});
-    // }
+    if(myAuthorization){
+        const token = headersList.get('authorization').replace('Bearer ', '');
+        const secret = new TextEncoder().encode(process.env.MY_AUTH_KEY);
+    
+        try {
+            const { payload: { email } } = await jose.jwtVerify(token, secret);
+            if (!listEmailApproved.includes(email)) {
+                throw new Error();
+            }
+        } catch(err) {
+            console.log(err);
+            return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Methods": "GET, POST", // If you're making POST requests
+            }});
+        }
+    }else if(request.method === 'OPTIONS'){
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        }
+        return new NextResponse({ status: 200, headers });
+    }else{
+        return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        }});
+    }
 
 }
 
