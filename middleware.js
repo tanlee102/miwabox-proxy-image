a9b7ff007232c824127ae5cfb516965278cf3c46
LE QUOC TAN <letanprox@gmail.com>
2024-05-13 23:25:01 +0700

    U

--- middleware.js
+++ middleware.js
@@ -10,40 +10,39 @@ const listEmailApproved = [
 export async function middleware(request) {
 
     const headersList = headers()
-    const myAuthorization = headersList.get('mytoken');
+    const token = headersList.get('mytoken');
 
-    console.log(headersList)
-    console.log(request.method, myAuthorization)
+    console.log(request.method, token)
     console.log('-------')
 
-    // if(myAuthorization){
+    if(token){
 
-    //     const token = headersList.get('mytoken').replace('Bearer ', '');
-    //     const secret = new TextEncoder().encode(process.env.MY_AUTH_KEY);
+        const secret = new TextEncoder().encode(process.env.MY_AUTH_KEY);
     
-    //     // try {
-    //     //     const { payload: { email } } = await jose.jwtVerify(token, secret);
-    //     //     if (!listEmailApproved.includes(email)) {
-    //     //         throw new Error();
-    //     //     }
-    //     // } catch(err) {
-    //     //     console.log(err);
-    //     //     return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
-    //     //         "Content-Type": "*",
-    //     //         "Access-Control-Allow-Origin": "*",
-    //     //         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    //     //         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    //     //     }});
-    //     // }
-    // }else if(request.method === 'OPTIONS'){
-    //     const headers = {
-    //         "Content-Type": "application/json, text/plain, */*",
-    //         "Access-Control-Allow-Origin": "*",
-    //         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    //         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    //     }
-    //     return new NextResponse({ status: 200, headers });
-    // }else{
+        try {
+            const { payload: { email } } = await jose.jwtVerify(token, secret);
+            if (!listEmailApproved.includes(email)) {
+                throw new Error();
+            }
+        } catch(err) {
+            console.log(err);
+            return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
+                "Content-Type": "application/json, text/plain, */*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            }});
+        }
+    }else if(request.method === 'OPTIONS'){
+        const headers = {
+            "Content-Type": "application/json, text/plain, */*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        }
+        return new NextResponse({ status: 200, headers });
+    }
+    // else{
     //     return NextResponse.json({ error: "Failed to authenticate." }, { status: 400, headers: {
     //         "Content-Type": "application/json, text/plain, */*",
     //         "Access-Control-Allow-Origin": "*",
