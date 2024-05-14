901d7fd621265a3e25626922722810225d7d121a
LE QUOC TAN <letanprox@gmail.com>
2024-05-14 07:11:11 +0700

    J

--- middleware.js
+++ middleware.js
@@ -1,17 +1,34 @@
 import { NextResponse } from 'next/server'
 import { headers } from 'next/headers'
- 
+import * as jose from 'jose'
+
 const listEmailApproved = [
     'xemtua@gmail.com',
     'hoifancuongonepiece@gmail.com'
 ]
 
-export function middleware(request) {
+export async function middleware(request) {
 
     if(request.method === 'PUT'){
         const headersList = headers()
         const token = headersList.get('authorization').replace('Bearer ', '');
-        console.log(token)
+        const secret = new TextEncoder().encode(process.env.MY_AUTH_KEY);
+
+        if (!pathName.startsWith(matchString)) {
+            try {
+                const { payload } = await jose.jwtVerify(token, secret);
+
+                console.log(payload)
+
+                if (!listEmailApproved.includes(payload.email)) {
+                    throw new Error();
+                }
+            } catch(err) {
+                console.log(err);
+                return NextResponse.json({ error: "Failed to authenticate." }, { status: 400 });
+            }
+        }
+
     }
 
     return NextResponse.next()
