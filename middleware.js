0a66d5ef90d8230e2443acb829324f24f44d1b61
LE QUOC TAN <letanprox@gmail.com>
2024-05-14 07:43:37 +0700

    J

--- middleware.js
+++ middleware.js
@@ -27,6 +27,15 @@ export async function middleware(request) {
 
     }
 
+    if(request.method === 'OPTIONS'){
+        return new NextResponse({ status: 400, headers: {
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        }});
+    }
+
     return NextResponse.next()
 
 }
