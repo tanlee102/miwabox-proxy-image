6aa6ef11e9d0803b512f6f7791248c0263f06fda
LE QUOC TAN <letanprox@gmail.com>
2024-05-14 07:04:44 +0700

    J

--- middleware.js
+++ middleware.js
@@ -1,8 +1,18 @@
 import { NextResponse } from 'next/server'
+import { headers } from 'next/headers'
  
+const listEmailApproved = [
+    'xemtua@gmail.com',
+    'hoifancuongonepiece@gmail.com'
+]
 
 export function middleware(request) {
 
+    if(request.method === 'PUT'){
+        const headersList = headers()
+        const token = headersList.get('authorization').replace('Bearer ', '');
+        console.log(token)
+    }
 
     return NextResponse.next()
 
